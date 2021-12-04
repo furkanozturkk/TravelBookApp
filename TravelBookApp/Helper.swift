b9469c3f0dd4ae47e21f3190b366922f59588639
Muhammet Furkan ÖZTÜRK <m.furkanozturkk@gmail.com>
2021-12-04 13:59:37 +0300

    first commit

---
+++ TravelBookApp/Helper.swift
@@ -0,0 +1,29 @@
+//
+//  Helper.swift
+//  TravelBookApp
+//
+//  Created by Furkan Öztürk on 4.12.2021.
+//
+
+import UIKit
+
+class Helper: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
